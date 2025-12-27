d2fd293f8075fc5792f806a59c9ac25b84793933
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-12-27 16:03:37 +0530

    Responding with ping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::io::prelude::*;
+use std::io::{BufReader,Write};
+use std::net::TcpListener;
 use std::net::TcpStream;
+fn handle_connection(mut stream:TcpStream){
+    let mut bufread=BufReader::new(&mut stream);
+    stream.write_all("Ping".as_bytes());
+}
 fn main()->std::io::Result<()> {
-    let mut stream=TcpStream::connect("127.0.0.1:6379")?;
+    let listener=TcpListener::bind("127.0.0.1:6379")?;
+    for stream in listener.incoming(){
+        let stream=stream.unwrap();
+        handle_connection(stream);
+    }
     Ok(())
 }
