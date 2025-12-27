c8d3e81652943a768328a30af622014bc3d74eb6
ken-kaneki-9 <160580260+ken-kaneki-9@users.noreply.github.com>
2025-12-27 16:22:35 +0530

    concurrent hanlding, responds with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::thread;
 use std::io::{BufReader,Write};
 use std::net::TcpListener;
 use std::net::TcpStream;
 fn handle_connection(mut stream:TcpStream){
     let mut bufread=BufReader::new(&mut stream);
-    stream.write_all("Ping".as_bytes());
+    stream.write_all("+PONG\r\n".as_bytes());
 }
 fn main()->std::io::Result<()> {
     let listener=TcpListener::bind("127.0.0.1:6379")?;
     for stream in listener.incoming(){
         let stream=stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(move ||{handle_connection(stream)});
     }
     Ok(())
 }
